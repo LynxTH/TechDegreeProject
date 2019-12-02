4273a0a0a5472e5f77a24bf333221a4c347f696b
LynxTH <58054598+LynxTH@users.noreply.github.com>
2019-12-02 18:37:13 -0500

    Updated JS code

--- js/script.js
+++ js/script.js
@@ -30,4 +30,70 @@ project 1 - A Random Quote Generator
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
 
+var message = '';
+var quotes;
+var source;
+var citation;
+var Year;
+
+
+var quoteList = [
+    {quotes: 'WOOO!!', 
+     source: "-Ellen, Queen of Durham",
+     Year: 2018},
+
+    {quotes: "Im your huckleberry", 
+     source: "-Doc Holiday",
+     citation: "Tombstone",
+     Year: 1993},
+
+    {quotes: "In a dark place we find oursleves,  and a little more knowledge lights our way.",
+    source: '-Yoda'},
+
+    {quotes: "No one likes a grumpy muffin.",
+     source: '-Ellen, Queen of Durham',
+     Year: 2019},
+
+    {quotes: "Nothing will work unless you do", 
+     source: 'Maya Angelou'},
+
+    {quotes: "I learned that courage is not the absence of fear but the triumph over it",
+     source: 'Nelson Mandela'},
+]
+
+function getRandomQuote () {
+
+  var getRandomNumber = quoteList[Math.floor(Math.random() * quoteList.length)];
+  return getRandomNumber;
+ }
+ getRandomQuote();
+console.log(getRandomQuote());
+
+function printQuote () {
+
+    var HTMLString = '<p class = "qoute">' + getRandomQuote + '</p>';
+    HTMLString += '<p class = "source"> + [quoteList].quotes' + '</p>';
+    if ([quoteList].citation) {
+        HTMLString += '<span class="citation">' + quotes[quoteList].citation + '</span>';
+    }
+    if ([quoteList].Year) {
+        HTMLString += '<span class = "Year">' + quote[quoteList].Year + '</p>';
+    }
+    message += '<h2>' + getRandomQuote() + '</h2>';
+ console.log(HTMLString);   
+ return getRandomQuote();
+}
+printQuote();
+console.log(printQuote());
+
+
+
+function print(message) {
+  var quoteBoxDiv = document.getElementById("quoteBox");
+  quoteBoxDiv.innerHTML = message;
+}
+
+print(message);
+document.write(getRandomQuote);
+
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
